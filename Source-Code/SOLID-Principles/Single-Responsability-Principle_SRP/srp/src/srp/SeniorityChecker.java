7f4b56407bc363f388ad15de3192059e7dd2064f
Kairos Isreel Znitch Garcia <nem.igarcia@coppel.com>
2023-11-24 16:52:29 -0600

    Adding SRP principle

---
+++ Source-Code/SOLID-Principles/Single-Responsability-Principle_SRP/srp/src/srp/SeniorityChecker.java
@@ -0,0 +1,9 @@
+package srp;
+
+public class SeniorityChecker {
+
+    public String checkSeniority(double experienceInYears){
+        return experienceInYears > 5 ? "senior": "junior";
+    }
+
+}
